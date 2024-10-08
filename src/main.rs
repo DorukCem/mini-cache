257594d61ae44bff0dfc1a421dcfda0606323d4f
Doruk <doruk.cem@gmail.com>
2024-10-08 21:10:50 +0300

    multiple pings

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
-use std::{io::{BufReader, Write}, net::{TcpListener, TcpStream}};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::{TcpListener, TcpStream},
+};
+
+use const_format::concatcp;
+
+const PORT: &'static str = "7879";
+const ADDRESS: &'static str = concatcp!("127.0.0.1:", PORT);
 
 fn start_tcp_server() {
-    let listener = TcpListener::bind("127.0.0.1:6379").expect("Cannot bind to port");
+    let listener = TcpListener::bind(ADDRESS).expect("Cannot bind to port");
+    println!("Server listening on {ADDRESS}");
 
     for stream in listener.incoming() {
         match stream {
@@ -17,11 +26,20 @@ fn start_tcp_server() {
     println!("Connection established!");
 }
 
-fn handle_connection(mut stream: TcpStream){
-    let _buf_reader = BufReader::new(&stream);
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let request: Vec<_> = buf_reader
+        .lines()
+        .flatten()
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-    let response = "+PONG\r\n";
-    stream.write_all(response.as_bytes()).unwrap();
+    println!("{:#?}", &request);
+    for command in request {
+        let response = "+PONG\r\n";
+        stream.write_all(response.as_bytes()).unwrap();
+    }
+
 }
 
 fn main() {
@@ -45,16 +63,12 @@ mod tests {
         // Give the server some time to start up
         thread::sleep(Duration::from_secs(1));
 
-        let server_address = "127.0.0.1:6379";
+        let server_address = ADDRESS;
         let connection =
             TcpStream::connect_timeout(&server_address.parse().unwrap(), Duration::from_secs(1));
 
-        assert!(
-            connection.is_ok(),
-            "Failed to connect to the TCP server on port 6379"
-        );
+        assert!(connection.is_ok(), "Failed to connect to the TCP server");
     }
-
 
     #[test]
     fn test_ping_pong_response() {
@@ -64,19 +78,10 @@ mod tests {
 
         thread::sleep(Duration::from_secs(1));
 
-        let mut stream = TcpStream::connect("127.0.0.1:6379")
-            .expect("Failed to connect to the TCP server on port 6379");
-
-        let ping_message = b"PING\r\n";
-        stream.write_all(ping_message).expect("Failed to send PING command");
-
-        let mut buffer = [0; 512];
-        let size = stream.read(&mut buffer).expect("Failed to read response from server");
-
-        // Convert the response to a string for validation
-        let response = String::from_utf8_lossy(&buffer[..size]);
-
-        // Check if the response matches the expected PONG response
-        assert_eq!(response, "+PONG\r\n", "The server did not respond with +PONG\\r\\n");
+        let mut stream = std::net::TcpStream::connect(ADDRESS).unwrap();
+        stream.write(b"PING").unwrap();
+        let mut buffer = [0; 516];
+        stream.read(&mut buffer).unwrap();
+        assert_eq!(buffer.as_slice(), b"+PONG\r\n");
     }
 }
