716d0cc4e91a86a4d3c30e9608bc6337adbf8b70
Doruk <doruk.cem@gmail.com>
2024-10-08 22:03:16 +0300

    echo commands

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,46 @@
-use std::{
-    io::{BufRead, BufReader, Write},
-    net::{TcpListener, TcpStream},
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 use const_format::concatcp;
 
 const PORT: &'static str = "7879";
 const ADDRESS: &'static str = concatcp!("127.0.0.1:", PORT);
 
-fn start_tcp_server() {
-    let listener = TcpListener::bind(ADDRESS).expect("Cannot bind to port");
-    println!("Server listening on {ADDRESS}");
+async fn start_tcp_server() {
+    let listener = TcpListener::bind(ADDRESS).await.unwrap();
 
-    for stream in listener.incoming() {
-        match stream {
-            Ok(stream) => {
-                handle_connection(stream);
-            }
-            Err(e) => {
-                println!("error: {}", e);
-            }
+    loop {
+        // The second item contains the IP and port of the new connection.
+        let (socket, _) = listener.accept().await.unwrap();
+        tokio::spawn(async move {
+            handle_connection(socket).await;
+        });
+    }
+}
+
+async fn handle_connection(mut socket: TcpStream) {
+    let mut buf = vec![0; 1024];
+
+    loop {
+        let n = socket
+            .read(&mut buf)
+            .await
+            .expect("failed to read data from socket");
+
+        if n == 0 {
+            return;
         }
+        
+        socket
+            .write_all(&buf[0..n])
+            .await
+            .expect("failed to write data to socket");
     }
-
-    println!("Connection established!");
 }
 
-fn handle_connection(mut stream: TcpStream) {
-    let buf_reader = BufReader::new(&stream);
-    let request: Vec<_> = buf_reader
-        .lines()
-        .flatten()
-        .take_while(|line| !line.is_empty())
-        .collect();
-
-    println!("{:#?}", &request);
-    for command in request {
-        let response = "+PONG\r\n";
-        stream.write_all(response.as_bytes()).unwrap();
-    }
-
-}
-
-fn main() {
-    start_tcp_server();
+#[tokio::main]
+async fn main() {
+    start_tcp_server().await;
 }
 
 #[cfg(test)]
