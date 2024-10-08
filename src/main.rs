b0ec48c0a8fc8b7913153d247d2627035b53e1d8
Doruk <doruk.cem@gmail.com>
2024-10-08 12:53:25 +0300

    respond to ping

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-use std::net::TcpListener;
+use std::{io::{BufReader, Write}, net::{TcpListener, TcpStream}};
 
 fn start_tcp_server() {
     let listener = TcpListener::bind("127.0.0.1:6379").expect("Cannot bind to port");
 
     for stream in listener.incoming() {
         match stream {
-            Ok(_stream) => {
-                println!("accepted new connection");
+            Ok(stream) => {
+                handle_connection(stream);
             }
             Err(e) => {
                 println!("error: {}", e);
@@ -17,6 +17,13 @@ fn start_tcp_server() {
     println!("Connection established!");
 }
 
+fn handle_connection(mut stream: TcpStream){
+    let _buf_reader = BufReader::new(&stream);
+
+    let response = "+PONG\r\n";
+    stream.write_all(response.as_bytes()).unwrap();
+}
+
 fn main() {
     start_tcp_server();
 }
@@ -24,6 +31,7 @@ fn main() {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::io::{Read, Write};
     use std::net::TcpStream;
     use std::thread;
     use std::time::Duration;
@@ -39,11 +47,36 @@ mod tests {
 
         let server_address = "127.0.0.1:6379";
         let connection =
-            TcpStream::connect_timeout(&server_address.parse().unwrap(), Duration::from_secs(5));
+            TcpStream::connect_timeout(&server_address.parse().unwrap(), Duration::from_secs(1));
 
         assert!(
             connection.is_ok(),
             "Failed to connect to the TCP server on port 6379"
         );
+    }
+
+
+    #[test]
+    fn test_ping_pong_response() {
+        thread::spawn(|| {
+            super::start_tcp_server();
+        });
+
+        thread::sleep(Duration::from_secs(1));
+
+        let mut stream = TcpStream::connect("127.0.0.1:6379")
+            .expect("Failed to connect to the TCP server on port 6379");
+
+        let ping_message = b"PING\r\n";
+        stream.write_all(ping_message).expect("Failed to send PING command");
+
+        let mut buffer = [0; 512];
+        let size = stream.read(&mut buffer).expect("Failed to read response from server");
+
+        // Convert the response to a string for validation
+        let response = String::from_utf8_lossy(&buffer[..size]);
+
+        // Check if the response matches the expected PONG response
+        assert_eq!(response, "+PONG\r\n", "The server did not respond with +PONG\\r\\n");
     }
 }
